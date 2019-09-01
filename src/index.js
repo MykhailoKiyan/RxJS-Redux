a3264dabc7d9bd689443b7fac3187dfbf076d3b3
Mikhail Kiyan <Kiyan@ukr.net>
2019-09-01 21:26:50 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+document.getElementById('increment').addEventListener('click', () => {
+  console.log('Increment key was clicked');
+});
+
+document.getElementById('decrement').addEventListener('click', () => {
+  console.log('Decrement key was clicked');
+});
+
+document.getElementById('add').addEventListener('click', () => {
+  console.log('Add key was clicked');
+});
